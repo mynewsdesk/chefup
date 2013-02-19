4cc3c3d1f5e44af7b9170a1394ce361fdcbc61f0
Jan Andersson <jan.andersson@gmail.com>
2013-02-19 11:06:50 +0100

    Set file_backup_path

--- solo.rb
+++ solo.rb
@@ -1,3 +1,4 @@
 base_dir = File.expand_path(File.dirname(__FILE__))
 file_cache_path File.join(base_dir, "tmp", "cache")
 cookbook_path File.join(base_dir, "cookbooks"), File.join(base_dir, "site-cookbooks")
+file_backup_path File.join(base_dir, "tmp", "backup")
