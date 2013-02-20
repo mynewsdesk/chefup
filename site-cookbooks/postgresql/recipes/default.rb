94ad1d140cb3fcaf953acea7532acc2b8d27aa6a
Jan Andersson <jan.andersson@gmail.com>
2013-02-20 23:33:34 +0100

    Add postgresql cookbook

---
+++ site-cookbooks/postgresql/recipes/default.rb
@@ -0,0 +1,17 @@
+include_recipe "homebrew"
+
+homebrew_tap "mynewsdesk/mnd" do
+  action :tap
+end
+
+package "mnd-postgresql"
+
+execute "initdb /usr/local/var/postgres" do
+  creates "/usr/local/var/postgres/PG_VERSION"
+end
+
+unless File.exists?("#{ENV['HOME']}/Library/LaunchAgents/homebrew.mxcl.mnd-postgresql.plist")
+  directory "#{ENV['HOME']}/Library/LaunchAgents"
+  execute "cp /usr/local/Cellar/mnd-postgresql/9.1/homebrew.mxcl.mnd-postgresql.plist #{ENV['HOME']}/Library/LaunchAgents/"
+  execute "launchctl load -w #{ENV['HOME']}/Library/LaunchAgents/homebrew.mxcl.mnd-postgresql.plist"
+end
