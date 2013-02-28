336571b5801ccb0f2fecbe73e369fff6db40d1e5
Jan Andersson <jan.andersson@gmail.com>
2013-02-28 14:56:14 +0100

    Add b function for bundle exec

--- site-cookbooks/rbenv/files/default/rbenv.sh
+++ site-cookbooks/rbenv/files/default/rbenv.sh
@@ -1,2 +1,10 @@
 # Enable rbenv
 if which rbenv > /dev/null; then eval "$(rbenv init -)"; fi
+
+# Avoid using `bundle exec`
+function b {
+  if [[ -a Gemfile ]]
+  then bundle exec $*
+  else command $*
+  fi
+}
